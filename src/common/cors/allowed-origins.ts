cbdd8d65bc45b662ea33176fa850c8a0a4d9a16f
Tim Rolsh <tim.rolsh@gmail.com>
2025-05-27 12:23:24 -0400

    Add gloop frontend vercel url to cors policy

--- src/common/cors/allowed-origins.ts
+++ src/common/cors/allowed-origins.ts
@@ -2,5 +2,6 @@ export const allowedOrigins = [
   "http://localhost:3000",
   "https://dev-gloop.vercel.app",
   "https://gloop-indol.vercel.app",
+  "https://gloop-frontend.vercel.app",
   "https://gloop.finance"
 ];
